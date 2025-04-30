dc3d0d3e79d15fd6d98afd43c9c5172063408e5c
tpavelv <tpavelv@icloud.com>
2025-04-30 18:45:06 +0800

    refactoring task-filter on hooks

--- src/components/task-filter/task-filter.js
+++ src/components/task-filter/task-filter.js
@@ -1,45 +1,44 @@
-import React, { Component } from 'react'
+import React from 'react'
 import PropTypes from 'prop-types'
 import './task-filter.css'
 
-export default class TaskFilter extends Component {
-  static defaultProps = {
-    activeFilter: 'all',
-  }
+const TaskFilter = (props) => {
+  const { changeFilter, activeFilter } = props
 
-  static propTypes = {
-    changeFilter: PropTypes.func.isRequired,
-    activeFilter: PropTypes.string,
-  }
-
-  static finalClassName(value, dataAtr) {
+  const finalClassName = (value, dataAtr) => {
     if (value === dataAtr) {
       return 'selected'
     }
     return ''
   }
 
-  render() {
-    const { changeFilter, activeFilter } = this.props
-
-    return (
-      <ul className="filters" onClick={(e) => changeFilter(e.target.dataset.type)}>
-        <li>
-          <button className={TaskFilter.finalClassName(activeFilter, 'all')} data-type="all">
-            All
-          </button>
-        </li>
-        <li>
-          <button data-type="active" className={TaskFilter.finalClassName(activeFilter, 'active')}>
-            Active
-          </button>
-        </li>
-        <li>
-          <button data-type="done" className={TaskFilter.finalClassName(activeFilter, 'done')}>
-            Completed
-          </button>
-        </li>
-      </ul>
-    )
-  }
+  return (
+    <ul className="filters" onClick={(e) => changeFilter(e.target.dataset.type)}>
+      <li>
+        <button className={finalClassName(activeFilter, 'all')} data-type="all">
+          All
+        </button>
+      </li>
+      <li>
+        <button data-type="active" className={finalClassName(activeFilter, 'active')}>
+          Active
+        </button>
+      </li>
+      <li>
+        <button data-type="done" className={finalClassName(activeFilter, 'done')}>
+          Completed
+        </button>
+      </li>
+    </ul>
+  )
 }
+
+TaskFilter.defaultProps = {
+  activeFilter: 'all',
+}
+
+TaskFilter.propTypes = {
+  changeFilter: PropTypes.func.isRequired,
+  activeFilter: PropTypes.string,
+}
+export default TaskFilter
