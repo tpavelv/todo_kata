386c12e8827b1ba7eda233501a75693fe7a9b3c9
tpavelv <tpavelv@icloud.com>
2025-05-01 14:26:08 +0800

    timer on hooks

--- src/components/timer/timer.js
+++ src/components/timer/timer.js
@@ -1,14 +1,12 @@
-import React, { Component } from 'react'
+import React, { useState, useRef, useEffect } from 'react'
 import './timer.css'
 
-export default class Timer extends Component {
-  state = {
-    showTime: this.props.timer,
-  }
+const Timer = ({ timer }) => {
+  const [showTime, setShowTime] = useState(timer)
+  const ref = useRef(null)
+  let timerId = ref.current
 
-  timerId = null
-
-  static convertTime(ms) {
+  const convertTime = (ms) => {
     const totalSeconds = Math.round(ms / 1000)
     const minutes = Math.floor(totalSeconds / 60)
     const seconds = totalSeconds % 60
@@ -19,44 +17,41 @@ export default class Timer extends Component {
     return `${paddedMinutes}:${paddedSeconds}`
   }
 
-  onStartTimer = () => {
-    if (this.timerId) {
-      clearInterval(this.timerId)
+  const onStartTimer = () => {
+    if (timerId) {
+      clearInterval(timerId)
     }
 
     const startTime = Date.now()
-    const endTime = startTime + this.state.showTime
-    this.timerId = setInterval(() => {
+    const endTime = startTime + showTime
+    timerId = setInterval(() => {
       const nowTime = Date.now()
       const remainingTime = endTime - nowTime
       if (remainingTime <= 1000) {
-        clearInterval(this.timerId)
-        this.timerId = null
-        this.setState({ showTime: 0 })
+        clearInterval(timerId)
+        timerId = null
+        setShowTime(0)
       } else {
-        this.setState({ showTime: remainingTime })
+        setShowTime(remainingTime)
       }
     }, 1000)
   }
 
-  componentWillUnmount() {
-    clearInterval(this.timerId)
-    this.timerId = null
+  const onPausedTimer = () => {
+    clearInterval(timerId)
+    timerId = null
   }
 
-  onPausedTimer = () => {
-    clearInterval(this.timerId)
-    this.timerId = null
-  }
+  useEffect(() => () => clearInterval(timerId), [])
 
-  render() {
-    return (
-      <span className="timer">
-        <button className="icon icon-play " onClick={this.onStartTimer}></button>
-        <button className="icon icon-pause" onClick={this.onPausedTimer}></button>
+  return (
+    <span className="timer">
+      <button className="icon icon-play " onClick={onStartTimer}></button>
+      <button className="icon icon-pause" onClick={onPausedTimer}></button>
 
-        {Timer.convertTime(this.state.showTime)}
-      </span>
-    )
-  }
+      {convertTime(showTime)}
+    </span>
+  )
 }
+
+export default Timer
